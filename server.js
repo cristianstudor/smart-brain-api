d3924e687e52110076eaf5002d3a1f3f2095a787
Cristian Tudor <cristi_tcs@live.com>
2023-02-18 17:45:34 +0200

    changed db connection for render migration

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import knex from 'knex';
-import bcrypt from 'bcrypt-nodejs';
-import signin from './controllers/signin.js';
-import register from './controllers/register.js';
-import profile from './controllers/profile.js';
-import image from './controllers/image.js';
+import express from "express";
+import cors from "cors";
+import knex from "knex";
+import bcrypt from "bcrypt-nodejs";
+import signin from "./controllers/signin.js";
+import register from "./controllers/register.js";
+import profile from "./controllers/profile.js";
+import image from "./controllers/image.js";
 
 const app = express();
 
@@ -13,28 +13,39 @@ app.use(cors());
 app.use(express.json());
 
 const db = knex({
-  client: 'pg',
+  client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
+    ssl: { rejectUnauthorized: false },
+    host: process.env.DATABASE_URL,
+    port: 5432,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PW,
+    database: process.env.DATABASE_DB
   }
 });
 
-app.get('/', (req, res) => db.select('*').from('users').then(users => res.json(users)));
+app.get("/", (req, res) =>
+  db
+    .select("*")
+    .from("users")
+    .then((users) => res.json(users))
+);
 
-app.post('/signin', (req, res) => signin.handleSigninPost(req, res, db, bcrypt));
+app.post("/signin", (req, res) =>
+  signin.handleSigninPost(req, res, db, bcrypt)
+);
 
-app.post('/register', (req, res) => register.handleRegisterPost(req, res, db, bcrypt));
+app.post("/register", (req, res) =>
+  register.handleRegisterPost(req, res, db, bcrypt)
+);
 
-app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db));
+app.get("/profile/:id", (req, res) => profile.handleProfileGet(req, res, db));
 
-app.post('/imageurl', (req, res) => image.handleApiCall(req, res));
+app.post("/imageurl", (req, res) => image.handleApiCall(req, res));
 
-app.put('/image', (req, res) => image.handleImagePut(req, res, db));
+app.put("/image", (req, res) => image.handleImagePut(req, res, db));
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`app is running on port ${process.env.PORT || 3000}`)
+  console.log(`app is running on port ${process.env.PORT || 3000}`);
 });
- 
